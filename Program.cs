1dd4ce158abf48c3c659179ef48e384e2bc5c355
Larry González <larry.g.m1024@gmail.com>
2022-10-09 10:54:15 -0300

    my first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿ internal class Program
+    {
+         struct tLibro{
+                public string titulo;
+                public string autor;
+                public string editorial;
+              
+            };
+            tLibro[] biblioteca;
+            void cargarLibros(){
+                for(int i = 0; i < biblioteca.Length; i++){
+                    Console.Write("Ingresa el nombre del libro:");
+                    biblioteca[i].titulo = Console.ReadLine();
+                    Console.Write("Ingresa el autor del libro:");
+                    biblioteca[i].autor = Console.ReadLine();
+                    Console.Write("Ingresa la editorial del libro:");
+                    biblioteca[i].editorial = Console.ReadLine();
+                }
+            }
+
+            void mostrarlibros(){
+                foreach (tLibro libro in biblioteca)
+                {
+                    Console.WriteLine($"Titulo del libro: {libro.titulo}");
+                    Console.WriteLine($"Autor del libro: {libro.autor}");
+                    Console.WriteLine($"Editorial del libro: {libro.editorial}");
+                }
+                Console.WriteLine();
+
+                for(int i=0; i<biblioteca.Length;i++){
+                    Console.WriteLine($"Titulo del libro: {biblioteca[i].titulo}");
+                    Console.WriteLine($"Autor del libro: {biblioteca[i].autor}");
+                    Console.WriteLine($"Editorial del libro: {biblioteca[i].editorial}");
+                }
+            }
+        static void Main(string[] args)
+        {
+           const int maxcantidadLibros = 2;
+           var prog = new Program();
+           prog.biblioteca = new tLibro[maxcantidadLibros];
+
+           prog.cargarLibros();
+           prog.mostrarlibros();
+        }
+    }
